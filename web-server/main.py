3d392d376a664016a419c0a72c074d2421abaaf1
randy-urriola <randy.urriolah@gmail.com>
2025-09-12 14:50:53 -0500

    Docker

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,26 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse  # HTML Format
+
+# instance
+app = FastAPI()
+
+# Main page
+
+
+@app.get('/')  # decorator
+def get_list():
+    return [1, 2, 3, 4]
+
+# Contact page
+
+
+@app.get('/contact', response_class=HTMLResponse)  # decorator
+def get_list():
+    return '''
+    <h1>Hola, Titulo</h1>
+    <p>Hola, Párrafo</p>
+    '''
 
 
 def run():
